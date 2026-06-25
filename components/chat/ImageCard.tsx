31962ca1e7e7b41fa346f7b03df5b2c29ce1f996
weixin_48640477 <360913684@qq.com>
2026-06-25 15:07:07 +0800

    fix(chat/ImageCard): 为图片链接添加mini后缀加载缩略图
    修改了图片卡片组件的图片源地址，添加了-mini后缀来加载缩略图，优化图片加载表现

--- components/chat/ImageCard.tsx
+++ components/chat/ImageCard.tsx
@@ -61,7 +61,7 @@ export function ImageCard({ img, onDownload, onDelete, onDetails }: ImageCardPro
         ) : hasImage ? (
           <Image
             onClick={() => onDetails(img)}
-            src={img.image!.url}
+            src={img.image!.url+'-mini'}
             styles={{
               cover: {
                 backgroundColor: 'rgba(0, 0, 0, 0.15)'
